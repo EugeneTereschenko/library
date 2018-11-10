597f1a45bae7af99bc5f0105c0a57a64a52f9b28
EugeneTereschenko <tereschenko.eugene@gmail.com>
2018-11-10 21:56:04 +0200

    add keyword argrs

--- lib/entities/author.rb
+++ lib/entities/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_reader :name, :biography
-  def initialize(name, biography)
+  def initialize(name:, biography:)
     @name = name
     @biography = biography
     validate?
--- lib/entities/order.rb
+++ lib/entities/order.rb
@@ -1,7 +1,7 @@
 class Order
   attr_reader :book, :reader, :date
 
-  def initialize(book, reader, date)
+  def initialize(book:, reader:, date:)
     @book = book
     @reader = reader
     @date = date
