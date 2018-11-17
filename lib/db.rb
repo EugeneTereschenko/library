daee3761f9a1d531775effb8142287d236f4e8bb
EugeneTereschenko <tereschenko.eugene@gmail.com>
2018-11-17 18:09:11 +0200

    fix read file

--- lib/db.rb
+++ lib/db.rb
@@ -2,11 +2,19 @@ class Db
   DEFAULT_PATH_TO_FILE = 'database.yml'.freeze
 
   def initialize(path_to_file)
-    @path_to_file = path_to_file.empty? ? DEFAULT_PATH_TO_FILE : path_to_file
+   if path_to_file.empty?  
+    @path_to_file = DEFAULT_PATH_TO_FILE 
+   else
+    @path_to_file = path_to_file
+   end
   end
 
   def read_database
-    YAML.load_file(@path_to_file, nil)
+    if File.exist?(@path_to_file)
+     YAML.load_file(@path_to_file, nil)
+    else
+     nil
+    end
   end
 
   def write_database(library)
